9a05ff3c94c18c38aadd7dd4d99414eef9d6a3c4
wonderfulyanderful@gmail.com <wonderfulyanderful@gmail.com>
2024-02-17 18:46:35 -0800

    Added 3 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIzNTIzMQ/spectral.js?branch_slug=main&token=b90358bf-a4b9-406d-a210-8d6b181b60f2";
+export default { extends: ruleset };
